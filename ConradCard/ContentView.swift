35025d8e33d8a25a8aaa346c140ff74fd076a114
Conrad Taylor <conradwt@gmail.com>
2020-01-06 09:57:09 -0800

    Initial Commit

---
+++ ConradCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConradCard
+//
+//  Created by Conrad Taylor on 1/6/20.
+//  Copyright © 2020 Conrad Taylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
